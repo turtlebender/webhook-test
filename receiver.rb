10377c6b8741bdc5d9bcb35c595f4be217f5f393
Tom Howe <turtlebender@gmail.com>
2011-12-09 14:21:56 -0600

    updating receiver

--- receiver.rb
+++ receiver.rb
@@ -5,7 +5,8 @@ post '/webhook-test' do
 
   push = JSON.parse(params[:payload])
   environment = push['ref'].split "/"
+  puts "updating #{environment.last} nodes"
   `fix --env=#{environment.last} node:all`
-  
+    
 
 end
