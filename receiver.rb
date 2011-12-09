742bd1e2380752a6498eeba50ef54f7da87d7fb0
Tom Howe <turtlebender@gmail.com>
2011-12-09 14:32:04 -0600

    adding output

--- receiver.rb
+++ receiver.rb
@@ -5,7 +5,9 @@ post '/webhook-test' do
 
   push = JSON.parse(params[:payload])
   environment = push['ref'].split "/"
-  puts "updating #{environment.last} nodes"
+  puts "updating git"
+  `git pull`
+  puts "deploying nodes for #{environment.last} environment"
   `fix --env=#{environment.last} node:all`
     
 
